d64464d43e8694ea02ce6dddd28c10dc6b874b06
Anatol <anatoly.bludov@gmail.com>
2015-01-02 18:14:07 +0300

    Stub for arrays element utils swap method

---
+++ src/main/java/bludau/study/algorithms/api/utils/ArrayElementUtils.java
@@ -0,0 +1,10 @@
+package bludau.study.algorithms.api.utils;
+
+/**
+ * Helper for work with array elements.
+ * @author Anatol Bludau
+ *
+ */
+public interface ArrayElementUtils {
+	<T> T[] swap(T[] array, int firstElementIndex, int secondElementIndex);
+}
---
+++ src/main/java/bludau/study/algorithms/impl/utils/ArrayElementUtilsImpl.java
@@ -0,0 +1,13 @@
+package bludau.study.algorithms.impl.utils;
+
+import bludau.study.algorithms.api.utils.ArrayElementUtils;
+
+public class ArrayElementUtilsImpl implements ArrayElementUtils {
+
+	@Override
+	public <T> T[] swap(T[] array, int firstElementIndex, int secondElementIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
