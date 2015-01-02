071de32c55baa5074fd9eafb72bc734f8026f347
Anatol <anatoly.bludov@gmail.com>
2015-01-02 19:03:34 +0300

    Removed test suite. Added tests for swap elements method

--- src/test/java/bludau/study/algorithms/api/util/ArrayElementUtilsImplTest.java
+++ src/test/java/bludau/study/algorithms/api/util/ArrayElementUtilsImplTest.java
@@ -1,6 +1,8 @@
 package bludau.study.algorithms.api.util;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +12,40 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import bludau.study.algorithms.config.SpringConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SpringConfig.class})
+@ContextConfiguration(classes = { SpringConfig.class })
 public class ArrayElementUtilsImplTest {
-	
+
 	@Autowired
 	ArrayElementUtils utils;
-	
+
+	Integer[] inputArray;
+
+	@Before
+	public void before() {
+		inputArray = new Integer[] { 1, 2, 3, 4, 5 };
+	}
+
 	@Test
-	public void testSwap(){
-		Integer[] inputArray = new Integer[]{1, 2, 3, 4, 5};
-		Integer[] expectedResult = new Integer[]{1, 2, 4, 3, 5};
-		
+	public void testSwap() {
+		Integer[] expectedResult = new Integer[] { 1, 2, 4, 3, 5 };
+
 		utils.swap(inputArray, 2, 3);
 		assertArrayEquals(expectedResult, inputArray);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testSwapForNull() {
+		utils.swap(null, 3, 2);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testSwapForIndexOutOfBound() {
+		utils.swap(inputArray, 1000, 20);
+	}
+
+	@Test
+	public void testSwapReturnValue() {
+		Integer[] resultArray = utils.swap(inputArray, 2, 3);
+		assertSame(resultArray, inputArray);
 	}
 }
