e0950785d8d8f8c58da7b47462d06eed42e4bbb4
Anatol <anatoly.bludov@gmail.com>
2015-01-04 01:06:19 +0300

    Test for a simple case of array sorting for the selection sorting algorithm

--- src/test/java/bludau/study/algorithms/api/sorting/SortingProcessorSelectionImplTest.java
+++ src/test/java/bludau/study/algorithms/api/sorting/SortingProcessorSelectionImplTest.java
@@ -2,12 +2,17 @@ package bludau.study.algorithms.api.sorting;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.apache.commons.lang3.ArrayUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 import bludau.study.algorithms.api.sorting.qualifier.SelectionSortQualifier;
 import bludau.study.algorithms.config.SpringConfig;
@@ -25,6 +30,16 @@ public class SortingProcessorSelectionImplTest {
 	@Autowired
 	@SelectionSortQualifier
 	private SortingProcessor selectionSortProcessor;
+	
+	private Integer[] simpleArrayForSorting;
+	private Integer[] sortedSimpleArray;
+	
+	@Before
+	public void before(){
+		simpleArrayForSorting = new Integer[]{3, 2, 5, 4, 1};
+		sortedSimpleArray = ArrayUtils.clone(simpleArrayForSorting);
+		Arrays.sort(sortedSimpleArray);
+	}
 
 	/**
 	 * Check that result of sorting an empty array is empty array too.
@@ -52,8 +67,16 @@ public class SortingProcessorSelectionImplTest {
 	 */
 	@Test
 	public void testSortForReturnValueIsTheSameElement(){
-		Integer[] sourceArray = new Integer[]{1, 2, 3};
-		Integer[] resultArray = selectionSortProcessor.sort(sourceArray);
-		assertSame(sourceArray, resultArray);
+		Integer[] resultArray = selectionSortProcessor.sort(simpleArrayForSorting);
+		assertSame(simpleArrayForSorting, resultArray);
+	}
+	
+	/**
+	 * Check the simple case of sorting.
+	 */
+	@Test
+	public void testSortSimpleCase(){
+		Integer[] resultArray = selectionSortProcessor.sort(simpleArrayForSorting);
+		assertArrayEquals(sortedSimpleArray, resultArray);
 	}
 }
