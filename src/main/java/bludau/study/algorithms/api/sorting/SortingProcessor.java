07f5e7c59a267f626ba41d6e58c2f576b868dc4f
Anatol <anatoly.bludov@gmail.com>
2015-01-03 20:23:45 +0300

    Added API for sorting processor

---
+++ src/main/java/bludau/study/algorithms/api/sorting/SortingProcessor.java
@@ -0,0 +1,32 @@
+package bludau.study.algorithms.api.sorting;
+
+import java.util.Comparator;
+
+/**
+ * The sorting Processor.
+ * 
+ * @author Anatol Bludau
+ *
+ */
+public interface SortingProcessor {
+	/**
+	 * Sorts the input array. The original array is modified.
+	 * 
+	 * @param inputArray
+	 *            the input array
+	 * @return sorted array (the same object with input array)
+	 */
+	<T extends Comparable<T>> T[] sort(T inputArray);
+
+	/**
+	 * Sorts the input array using {@link Comparator}. The original array is
+	 * modified.
+	 * 
+	 * @param inputArray
+	 *            the input array
+	 * @param comparator
+	 *            the comparator for sorting
+	 * @return sorted array (the same object with input array)
+	 */
+	<T> T[] sort(T[] inputArray, Comparator<T> comparator);
+}
