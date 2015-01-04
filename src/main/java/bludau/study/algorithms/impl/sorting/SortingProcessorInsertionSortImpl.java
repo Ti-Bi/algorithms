19b5a663738a63cb3e46f45aa9ed20b1235ad8fc
Anatol <anatoly.bludov@gmail.com>
2015-01-04 14:49:16 +0300

    Draft version of the insertion sort

--- src/main/java/bludau/study/algorithms/impl/sorting/SortingProcessorInsertionSortImpl.java
+++ src/main/java/bludau/study/algorithms/impl/sorting/SortingProcessorInsertionSortImpl.java
@@ -2,9 +2,11 @@ package bludau.study.algorithms.impl.sorting;
 
 import java.util.Comparator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import bludau.study.algorithms.api.sorting.qualifier.InsertionSortQualifier;
+import bludau.study.algorithms.api.util.ArrayElementUtils;
 
 /**
  * The implementation of the insertion sorting processor.
@@ -16,6 +18,9 @@ import bludau.study.algorithms.api.sorting.qualifier.InsertionSortQualifier;
 @InsertionSortQualifier
 public class SortingProcessorInsertionSortImpl extends SortingProcessorAbstract {
 
+	@Autowired
+	private ArrayElementUtils arrayElementUtils;
+
 	/**
 	 * {@inheritDoc}
 	 * 
@@ -24,14 +29,17 @@ public class SortingProcessorInsertionSortImpl extends SortingProcessorAbstract 
 	 * </p>
 	 */
 	@Override
-	protected <T> T[] sortArray(T[] inputArray, Comparator<T> compareFunction) {
+	protected <T> T[] sortArray(T[] inputArray, Comparator<T> comparator) {
 
 		for (int i = 1; i < inputArray.length; i++) {
 			for (int j = i; j > 0; j--) {
-
+				if (comparator.compare(inputArray[j - 1], inputArray[j]) > 0) {
+					arrayElementUtils.swap(inputArray, j - 1, j);
+				}
 			}
 		}
-		return null;
+
+		return inputArray;
 	}
 
 }
