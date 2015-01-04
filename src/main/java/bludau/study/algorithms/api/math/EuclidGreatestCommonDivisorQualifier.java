a665b55cef307691218ad2c7bf2b6ddd444082a4
Anatol <anatoly.bludov@gmail.com>
2015-01-05 00:36:05 +0300

    API for Euclid's find greatest common division algorithm is added

---
+++ src/main/java/bludau/study/algorithms/api/math/EuclidGreatestCommonDivisorQualifier.java
@@ -0,0 +1,21 @@
+package bludau.study.algorithms.api.math;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/**
+ * The qualifier for the Euclid's find the greatest common divisor algorithm.
+ * 
+ * @author Anatol Bludau
+ *
+ */
+@Qualifier("bludau.study.algorithms.api.math.EuclidGreatestCommonDivisorQualifier")
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.FIELD })
+public @interface EuclidGreatestCommonDivisorQualifier {
+
+}
---
+++ src/main/java/bludau/study/algorithms/api/math/GreatestCommonDivisor.java
@@ -0,0 +1,21 @@
+package bludau.study.algorithms.api.math;
+
+/**
+ * Calculation of the greatest common division of two integer numbers.
+ * 
+ * @author Anatol Bludau
+ *
+ */
+public interface GreatestCommonDivisor {
+
+	/**
+	 * Finds the greatest common division of two integer numbers.
+	 * 
+	 * @param p
+	 *            first number
+	 * @param q
+	 *            second number
+	 * @return the greatest common division
+	 */
+	int findGCD(int p, int q);
+}
