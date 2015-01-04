b8128cc974c3ab370023bdf3aaed508ac77e1c29
Anatol <anatoly.bludov@gmail.com>
2015-01-05 01:20:24 +0300

    Stubs for the Euclid's algorithm implementation and test

---
+++ src/main/java/bludau/study/algorithms/impl/math/EuclidGreatestCommonDivisorImpl.java
@@ -0,0 +1,23 @@
+package bludau.study.algorithms.impl.math;
+
+import bludau.study.algorithms.api.math.GreatestCommonDivisor;
+
+/**
+ * The Euclid's implementation of {@link GreatestCommonDivisor} interface.
+ * 
+ * @author Anatol Bludau
+ *
+ */
+public class EuclidGreatestCommonDivisorImpl implements GreatestCommonDivisor {
+
+	/**
+	 * {@inheritDoc}
+	 * <p>The Euclid's algorithm implementation.</p>
+	 */
+	@Override
+	public int findGCD(int p, int q) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/test/java/bludau/study/algorithms/impl/math/EuclidGreatestCommonDivisorImplTest.java
@@ -0,0 +1,29 @@
+package bludau.study.algorithms.impl.math;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import bludau.study.algorithms.api.math.EuclidGreatestCommonDivisorQualifier;
+import bludau.study.algorithms.api.math.GreatestCommonDivisor;
+import bludau.study.algorithms.config.SpringConfig;
+
+/**
+ * A test class for the {@link EuclidGreatestCommonDivisorImpl}.
+ * 
+ * @author Anatol Bludau
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { SpringConfig.class })
+public class EuclidGreatestCommonDivisorImplTest {
+
+	/**
+	 * The Euclid's greatest common divisor algorithm implementation for
+	 * testing.
+	 */
+	@Autowired
+	@EuclidGreatestCommonDivisorQualifier
+	private GreatestCommonDivisor gcdFinder;
+}
